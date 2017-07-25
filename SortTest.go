ab94b6c7fddf188b6bccbc09ffa1218f059bf281
lihuaye <lihuaye@163.com>
2017-07-25 19:46:15 +0800

    快速排序第一次优化,针对在排序数组有序或基本有序下的处理

--- SortTest.go
+++ SortTest.go
@@ -5,21 +5,24 @@ import (
 )
 
 func main() {
-	n := 100000000
-	arr := mysort.GenerateRandomArray(n, 0, n)
-	//arr := mysort.GenerateBasicOrderArray(n, 0)
+	n := 100000
+	//arr := mysort.GenerateRandomArray(n, 0, n)
+	arr := mysort.GenerateBasicOrderArray(n, 0)
 	//arr := []int{7, 9, 1, 3, 5, 7, 89, 123, 453, 675, 765, 865, 324, 4645, 76573, 234, 34, 54353, 433, 12}
 	//mysort.TestSort("SelectionSort", arr, mysort.SelectionSort)
 	//mysort.TestSort("InsertionSort", arr, mysort.InsertionSort)
 	//utils.WriteRandom2File("hello.txt", arr)
 	//arr := utils.ReadRandom("hello.txt", 500000)
+	//fmt.Println(arr)
+	//mysort.TestSort("BubbleSort", arr, mysort.BubbleSort)
+	//fmt.Println(arr)
 	//mysort.TestSort("BubbleSort", arr, mysort.BubbleSort)
 	//mysort.TestSort("SelectionSort", arr, mysort.SelectionSort)
-	arr1 := make([]int, len(arr))
-	copy(arr1, arr)
+	//arr1 := make([]int, len(arr))
+	//copy(arr1, arr)
 	//mysort.TestSort("ShellSort", arr, mysort.ShellSort)
 	//mysort.TestSort("InsertionSort", arr1, mysort.InsertionSort)
-	mysort.TestSort("MergeSort", arr1, mysort.MergeSort)
+	//mysort.TestSort("MergeSort", arr1, mysort.MergeSort)
 	mysort.TestSort("QuickSort", arr, mysort.QuickSort)
 	//fmt.Println("=====")
 	//arr1 := mysort.GenerateBasicOrderArray(n, 100)
--- sort/QuickSort.go
+++ sort/QuickSort.go
@@ -1,5 +1,7 @@
 package mysort
 
+import "math/rand"
+
 //快速排序
 func QuickSort(arr []int) {
 	size := len(arr)
@@ -13,31 +15,25 @@ func QuickSort(arr []int) {
 
 //确定基点的位置
 func partition(arr []int, size int) int {
-	//基点下标
-	l := 0
+	//随机选取一个下标来做基点值
+	randIndex := rand.Intn(size)
+	//跟第一个元素进行交换
+	Swap(&arr[0], &arr[randIndex])
 	//小于基点值的最后一个下标
 	minIndex := 0
-	//大于基点值的最后一个下标
-	maxIndex := 0
 	//基点值
-	point := arr[l]
+	point := arr[0]
 	//重新排序,确定基点位置
-	for i := 0; i < size; i++ {
+	for i := 1; i < size; i++ {
 		//arr[i]小于基点值时
 		if arr[i] < point {
 			//小于基准点的下标+1,用来容纳新值
 			minIndex ++
-			//交换值
-			if maxIndex != 0 {
-				Swap(&arr[minIndex], &arr[i])
-				maxIndex++
-			}
-		} else {
-			maxIndex++
+			Swap(&arr[minIndex], &arr[i])
 		}
 	}
 	//将最后一位小于基点值的值和基点值进行交换构成 arr[0:minIndex] < 基点值 <arr[minIndex+1:]的数组
-	Swap(&arr[l], &arr[minIndex])
+	Swap(&arr[0], &arr[minIndex])
 	//返回基点应该在排序完成的数组中的下标
 	return minIndex
 }
